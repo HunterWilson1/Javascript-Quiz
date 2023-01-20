9b0069d6453b43d66ab21d65bbfbf5946cfd59d5
Hunter Wilson <huntertheguy123@gmail.com>
2023-01-20 09:16:28 -0600

    I added more to the start quiz function and added a little css but css is not my best thing so I am holding off on it. I started on the countdown timer function but did not finish it. I wrote the checkAnswer on line 47 in script js but I have not written the function yet.

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,56 @@
 const questions = [
     {
         question: 'What do you enclose a string variable in? ',
-        choices: ["a. quotes", "b. brackets", "c. dashes", "d. asterisks"],
-        answer: "a. quotes"
+        choices: ["1. quotes", "2. brackets", "3. dashes", "4. asterisks"],
+        answer: "1. quotes"
     },
 
     {
         question: "At what number does an array start at?",
-        choices: ["a. 9", "b. 7", "c. 1", "d. 0"],
-        answer: "d. 0"
+        choices: ["1. 9", "2. 7", "3. 1", "4. 0"],
+        answer: "4. 0"
     },
 
     {
         question: "What does DOM mean?",
-        choices: ["a. Dominos", "b. Document Object Model", "c. It's just a name", "d. none of the above"],
-        answer: "b. Document Object Model"
+        choices: ["1. Dominos", "2. Document Object Model", "3. It's just a name", "4. none of the above"],
+        answer: "2. Document Object Model"
     }
 ];
-const highSores = [];
-let timeRemaining = 60;
-let currentQuestion = 0;
+
+const startScreen = document.querySelector("#start-screen");
+const questionScreen = document.querySelector("#question-screen")
 
 document.getElementById('start-button').addEventListener('click', startQuiz)
 
 function startQuiz() {
-    
+    questionScreen.removeAttribute("hidden");
+    currentQ = 0;
+    showQuestion();
 }
+
+function showQuestion () {
+    let question = questions[currentQ];
+    let choices = question.choices;
+
+    let h2QuestionElement = document.querySelector("#q-text");
+    h2QuestionElement.textContent = question.question;
+
+    for (let i = 0; i < choices.length; i++) {
+        let choice = choices[i];
+        let choiceButton = document.querySelector("#choice" + i);
+        choiceButton.textContent = choice;
+
+    }
+}
+
+document.querySelector("#q-choices").addEventListener("click", checkAnswer);
+
+function countdownTimer() {
+    time --;
+    displayTime();
+    if (time < 1) {
+        endQuiz();
+    }
+}
+
